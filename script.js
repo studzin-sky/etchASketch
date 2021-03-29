02198d08e80b3f300d754c1f4c79d46d706f8be4
studzin-sky <the.stucc@gmail.com>
2021-03-29 17:54:55 +0200

    completed issue 1

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 /*
 My plan: 
-1. Create default 16 x 16 grid of divs using java script inside of the container const
+Issue 1. Create default 16 x 16 grid of divs using java script inside of the container const
     - code it in js
     - write a css declaration for those divs, (grid? flexbox?)
-2. Set up a hover effect to change color on the divs when you hover a mouse over it
-3. Add a button to reset the grid 
-4. add a button to ask the user if he wants a different layout and then when he choses the number it is created over the default grid
-5. github
+Issue 2. Set up a hover effect to change color on the divs when you hover a mouse over it
+Issue 3. Add a button to reset the grid 
+Issue 4. add a button to ask the user if he wants a different layout and then when he choses the number it is created over the default grid
+Issue 5. github
 */
 
+const container = document.querySelector('#container');
+
+window.addEventListener('load', setDefaultGrid);
+
+function setDefaultGrid() {
+    setGridSize(16);
+    fillGrid(16);
+}
+
+function setGridSize(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  }
 
 
-const container = document.getElementById('container');
-
+function fillGrid(size) {
+    for (let i = 0; i < size * size; i++) {
+      const gridElement = document.createElement("div");
+      gridElement.classList = "grid-element";
+      container.appendChild(gridElement);
+    }
+  }
 //Create a grid with DOM 
