f3dd608bc05dbaf6612e67aa3a50bb7e38a35b3a
studzin-sky <the.stucc@gmail.com>
2021-03-29 19:01:55 +0200

    hovering over changes color

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ Issue 5. github
 
 const container = document.querySelector('#container');
 
+
 window.addEventListener('load', setDefaultGrid);
 
+// set a default 16 x 16 grid
 function setDefaultGrid() {
     setGridSize(16);
     fillGrid(16);
@@ -27,7 +29,13 @@ function fillGrid(size) {
     for (let i = 0; i < size * size; i++) {
       const gridElement = document.createElement("div");
       gridElement.classList = "grid-element";
+      gridElement.addEventListener('mouseover', generateRandomColor);
       container.appendChild(gridElement);
     }
   }
-//Create a grid with DOM 
+
+// create a function that generates random color and changes the background of the object calling the function
+function generateRandomColor() {
+    let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    this.style.backgroundColor = randomColor;
+}
