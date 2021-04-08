b33f7e0b3290b01c04b2a80235a423f78ab1899d
studzin-sky <the.stucc@gmail.com>
2021-04-08 18:44:49 +0200

    finished issue 4

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const container = document.getElementById('container');
 const erase = document.getElementById('erase');
+const range = document.getElementById('range');
 const userGrid = document.getElementsByClassName('grid-element');
 
-window.addEventListener('load', setDefaultGrid);
+document.addEventListener('load', setDefaultGrid(16));
 
 // set a default 16 x 16 grid
-function setDefaultGrid() {
-    setGridSize(16);
-    fillGrid(16);
+function setDefaultGrid(x) {
+  setGridSize(x);
+  fillGrid(x);
 }
 
 function setGridSize(size) {
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  }
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+}
 
 
 function fillGrid(size) {
@@ -30,9 +31,30 @@ function generateRandomColor() {
     this.style.backgroundColor = randomColor;
 }
 
-// create a function and an event listener to reset colors on a click
-erase.addEventListener('click', function(){
+
+// Issue 3 create an event listener to reset colors on a click
+erase.addEventListener('click', function() {
   for (let i=0; i < userGrid.length; i++) {
     userGrid[i].style.backgroundColor = 'inherit';
   }
 });
+
+//Issue 4 create a button to change the size of the grid 
+range.addEventListener('click', function(){
+  let gridArray = Array.from(container.childNodes);
+  for (let i=0; i < gridArray.length; i++) {
+    gridArray[i].remove();
+  };
+  let number = window.prompt('Choose a number between 2 - 64', '16');
+  
+  if (number !== null) {
+    number = parseInt(number);
+    if (number < 2 || number > 64 || Number.isNaN(number)) {
+      alert('Enter a correct number!');
+    } else {
+      setDefaultGrid(number);
+    }
+  }
+
+});
+
